08c4a173dfa7b03ad92e227c581158267eb8aa72
Kyle Cuss <cussprogramming@gmail.com>
2026-08-07 15:20:32 -0230

    Project initialize

---
+++ Source/ARPGStateTrees/ARPGStateTrees.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ARPGStateTrees.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ARPGStateTrees, "ARPGStateTrees" );
